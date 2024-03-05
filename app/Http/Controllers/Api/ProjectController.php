16c18bdbde0128f9eafa2cb53f98d44ab781074a
albertomandirola <albymandi00@gmail.com>
2024-03-05 16:42:52 +0100

    milestone 2 done

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Project;
+use App\Models\Project; 
 
 class ProjectController extends Controller
 {
